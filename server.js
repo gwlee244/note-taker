ad0ed0b0df0839ca244d7c669a14fca6f32b0fad
Gene Lee <gwlee244@gmail.com>
2020-03-12 15:23:15 -0400

    initial files

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening port: ${PORT}`);
+  });
